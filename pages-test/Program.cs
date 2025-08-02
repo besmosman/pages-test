e31be25b6550867fcfcd495239043123a5e15c94
Besm <besmbesmbesm@gmail.com>
2025-08-02 13:11:29 +0200

    test

---
+++ pages-test/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace pages_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Directory.Exists("output"))
+                Directory.Delete("output");
+
+            Directory.CreateDirectory("output");
+            File.WriteAllText("output/index.html",
+@"<html>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>");
+        }
+    }
+}
